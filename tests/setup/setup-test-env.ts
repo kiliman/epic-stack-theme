369824a0b9a70d0010a8d4df26e2cc3852858fca
kiliman <kiliman@gmail.com>
2023-08-09 10:36:49 -0400

    🎉 Initial commit

---
+++ tests/setup/setup-test-env.ts
@@ -0,0 +1,23 @@
+import 'dotenv/config'
+import 'source-map-support/register.js'
+import './setup-env-vars.ts'
+
+import fs from 'fs'
+import { installGlobals } from '@remix-run/node'
+import { afterAll, afterEach, expect } from 'vitest'
+import { prisma } from '~/utils/db.server.ts'
+import { matchers } from './matchers.cjs'
+import { BASE_DATABASE_PATH, DATABASE_PATH } from './paths.ts'
+import { deleteAllData } from './utils.ts'
+
+expect.extend(matchers)
+
+installGlobals()
+fs.copyFileSync(BASE_DATABASE_PATH, DATABASE_PATH)
+
+afterEach(() => deleteAllData())
+
+afterAll(async () => {
+	await prisma.$disconnect()
+	await fs.promises.rm(DATABASE_PATH)
+})
